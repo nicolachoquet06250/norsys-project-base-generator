36960f0c4c3185b1325b479af78276ae9deb7933
Nicolas Choquet <nicolas.choquet.ext@orange.com>
2022-08-09 12:51:45 +0200

    Gestion des notification via HTML/JS si Notifications natives non compatibles ou non activées sur la machine courante

--- notifications/unix_nobus.go
+++ notifications/unix_nobus.go
@@ -3,6 +3,10 @@
 
 package notifications
 
+import (
+	"npbg/notify"
+)
+
 func Notify(title, message, appIcon string, appId *string) error {
 	return beeep.Notify(title, message, appIcon, appId)
 }
